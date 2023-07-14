c501b10829c50bca9ed435185be11ab247c11a4c
chakkun1121 <chakkun1121haruki@gmail.com>
2023-07-14 15:18:57 +0900

    使い方を追加

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function view_file() {
   const file = document.querySelector('input[type=file]').files[0];
   const reader = new FileReader();
   let output = document.getElementById('output');
-  output.innerHTML = "処理中、そのままお待ち下さい。万が一動かなければ<a href='https://my-site.chakkun1121haru.repl.co/form/' target='_target'>お問い合わせください</a>。";
+  output.innerHTML = "処理中、そのままお待ち下さい。";
   reader.addEventListener("load", function() {
     const fileURL = reader.result;
     output.innerText = fileURL
